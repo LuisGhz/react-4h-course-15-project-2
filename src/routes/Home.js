cd3f6d475bbfe74e41fe6ce813937231039dd485
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-19 14:54:31 -0500

    Create Home component Set class container Set row and center align first child Create two divs with col and s5,s7 classes Import Form component and set it into first div

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import Form from 'components/Form';
+
+const Home = () => {
+  return (
+    <div className="container" >
+      <div className="row center-align">
+        <div className="col s7"><Form /></div>
+        <div className="col s5"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
