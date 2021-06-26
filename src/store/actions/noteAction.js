9e4929044fe16dc038d879cbf67d1a148d48845c
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-26 14:13:54 -0500

    Create deleteNote action

--- src/store/actions/noteAction.js
+++ src/store/actions/noteAction.js
@@ -13,3 +13,14 @@ export const addNote = note => {
     .catch(err => console.error(`Something was wrong: ${ err }`))
   }
 }
+
+export const deleteNote = note => {
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore()
+    firestore.collection('notes').doc(note.id).delete()
+    .then(() => {
+      console.log('Note deleted successfully');
+    })
+    .catch(err => console.error(`Something was wrong: ${ err }`))
+  }
+}
