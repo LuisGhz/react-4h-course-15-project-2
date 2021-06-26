cf461f3ac5f814441e9eb4c0484afe79c6b275a9
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-26 14:28:35 -0500

    Import moment js Show created date from now

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,6 @@
-import { deleteNote, toggleFavoriteNote } from "store/actions/noteAction";
 import { useDispatch } from "react-redux";
+import moment from 'moment';
+import { deleteNote, toggleFavoriteNote } from "store/actions/noteAction";
 
 const Note = ({ note }) => {
   const dispatch = useDispatch();
@@ -21,7 +22,7 @@ const Note = ({ note }) => {
       </div>
       <h5 className="black-text">{ note.title }</h5>
       <p className="truncate">{ note.content }</p>
-      <p className="grey-text">2 days ago</p>
+      <p className="grey-text">{ moment(note.createdAt.toDate()).fromNow() }</p>
       <div className="right-align">
         <i className="material-icons black-text" style={{ cursor: 'pointer' }}>edit</i>
       </div>
