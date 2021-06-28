7d4f4d1fb2c467078cefbea75d44cbf1156699ef
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-28 14:13:40 -0500

    Add path to NoteDetail

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from 'routes/Home'
 import NavBar from 'components/NavBar';
 import Favorites from 'components/Favorites';
+import NoteDetail from 'components/NoteDetail';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/note/:id" component={ NoteDetail } />
         </Switch>
       </Router>
     </div>
