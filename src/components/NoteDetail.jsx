8ea4c19be416d49bf053e6126a37cecba47c66c8
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-01 11:31:36 -0500

    Show different messages depending of note status. Show loading, not is empty and card content if exists

--- src/components/NoteDetail.jsx
+++ src/components/NoteDetail.jsx
@@ -6,20 +6,37 @@ const NoteDetail = props => {
   const id = props.match.params.id;
   useFirestoreConnect([{ collection: 'notes', doc: id }]);
   const note = useSelector(({ firestore: { data } }) => data.notes && data.notes[id]);
-
-  return (
+  const noteMarkup = !isLoaded(note) ? (
     <div className="container section">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">{ note?.title }</span>
-          <p>{ note?.content }</p>
+          <span className="card-title">Loading...</span>
+        </div>
+      </div>
+    </div>
+  ) : isEmpty(note) ? (
+    <div className="container section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Note content is empty</span>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="container section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">{ note.title }</span>
+          <p>{ note.content }</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
-          <div>{ moment( note?.createdAt.toDate() ).calendar() }</div>
+          <div>{ moment( note.createdAt.toDate() ).calendar() }</div>
         </div>
       </div>
     </div>
   );
+
+  return noteMarkup;
 }
 
 export default NoteDetail;
