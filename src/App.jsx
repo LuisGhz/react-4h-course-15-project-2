661ae57131c6706d52c05b33e5eea42785e80230
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-01 12:35:08 -0500

    Set edit note path with EditForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from 'routes/Home'
+import EditForm from 'routes/EditForm';
 import NavBar from 'components/NavBar';
 import Favorites from 'components/Favorites';
 import NoteDetail from 'components/NoteDetail';
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={ Home } />
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/note/:id" component={ NoteDetail } />
+          <Route exact path="/edit-note/:id" component={ EditForm } />
         </Switch>
       </Router>
     </div>
