19e678de594e725371be936a4b796619b5a949f2
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-01 13:21:08 -0500

    Create edit form

--- src/routes/EditForm.jsx
+++ src/routes/EditForm.jsx
@@ -1,9 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { editNote } from 'store/actions/noteAction';
+import useInput from 'customHook/useInput';
 
 const EditForm = () => {
+  const note = useSelector(state => state.note);
+  const dispatch = useDispatch();
+  const [title, bindTitle, resetTitle] = useInput(note.title);
+  const [content, bindContent, resetContent] = useInput(note.content);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(editNote({ id: note.id, title, content }));
+    resetTitle();
+    resetContent();
+  }
+
   return (
-  <div>
-    EditForm
-  </div>);
+    <div className="section">
+      <form  onSubmit={ handleSubmit } className="white" >
+        <h5 className="grey-text text-darken-3">Edit note note </h5>
+          <div className="input-field">
+            <input id="note-title" type="text" className="validate" { ...bindTitle } />
+            <label className="active" htmlFor="note-title">Note Title</label>
+          </div>
+          <div className="input-field">
+            <textarea id="note-content" className="materialize-textarea" { ...bindContent } ></textarea>
+            <label htmlFor="note-content">Note Content</label>
+          </div>
+          <button className="btn green">Edit note</button>
+      </form>
+    </div>
+  );
 }
 
 export default EditForm;
