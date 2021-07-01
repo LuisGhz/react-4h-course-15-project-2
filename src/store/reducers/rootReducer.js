e9e5a8c6a22db854e5a92a56de9f427e81d1b1cc
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-01 13:06:06 -0500

    Import noteReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
+import NoteReducer from './NoteReducer';
 
 const rootReducer = combineReducers({
+  note: NoteReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer
 });
